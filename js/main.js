9ba88d91de0d1053fac05f13417e379a36308f03
Valentin Bondyashev <valik.bond@mail.ru>
2018-04-26 09:37:48 +0300

    two

--- js/main.js
+++ js/main.js
@@ -2,8 +2,17 @@ $(function(){
 	$('.food_slide').slick({
 	infinite: true,
 	slidesToShow: 3,
-	slidesToScroll: 3,
-	appendArrows: '.portfolio-slider_but',
-	dots: true
+	slidesToScroll: 1,
+	prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
+	nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
+	dots: false
+	});
+
+	$(".food_btn").click(function(){
+		if (!$(".food_content").hasClass('clicked')) {
+			$(".food_content").addClass('clicked'); 
+		  } else { 
+			$(".food_content").removeClass('clicked');
+		  }
 	});
 });
